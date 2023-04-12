4519ca2a8804dfa1ada30e294ddb710dc8d8fdb4
Himanshu Gohil <himanshugohil234@gmail.com>
2023-04-12 22:37:58 +0530

    Added React to the project and auto reload for chrome extension

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+ReactDOM.createRoot(document.querySelector("#app") as HTMLElement).render(
+  <React.StrictMode>
+    <h1>Anitab</h1>
+  </React.StrictMode>
+);

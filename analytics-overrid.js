9735f5b088c2597f36575435460a9075b01fd607
tghimanshu <tghimanshu@users.noreply.github.com>
2023-04-22 12:57:43 +0000

    deploy: f027c4f50e0a69fa9ca2f8918d062e4707e9e886

---
+++ analytics-overrid.js
@@ -0,0 +1,20 @@
+// var _gaq = _gaq || [];
+// _gaq.push(["_setAccount", "G-WQR0VX9201"]);
+// _gaq.push(["_trackPageview", "/index.html"]);
+
+var _gaq = _gaq || [];
+_gaq.push(["_setAccount", "G-79Q6EPD4SY"]);
+_gaq.push(["_trackPageview"]);
+
+// "content_security_policy": {
+//   "extension_pages": "script-src 'self' https://ssl.google-analytics.com; object-src 'self'"
+// }
+
+// (function () {
+//   var ga = document.createElement("script");
+//   ga.type = "text/javascript";
+//   ga.async = true;
+//   ga.src = "https://ssl.google-analytics.com/ga.js";
+//   var s = document.getElementsByTagName("script")[0];
+//   s.parentNode.insertBefore(ga, s);
+// })();
